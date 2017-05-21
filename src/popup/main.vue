ef147ffaaf1354602bd2edb805ffaffc8679e5af
Saiya <evecalm@live.com>
2017-05-21 21:44:51 +0800

    init

---
+++ src/popup/main.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="popup">
+  <h1>{{ title }}</h1>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'main',
+  data () {
+    return {
+      title: 'abc'
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+h1 {
+  color: red;
+
+  span {
+    font-size: 16px;
+  }
+}
+</style>
